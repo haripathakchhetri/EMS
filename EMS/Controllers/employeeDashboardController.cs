681ad0fc1601588b855283e750c03ecb4ba1041d
haripathakchhetri <pathakhari570@gmail.com>
2023-05-10 09:18:53 +0545

    first commit

---
+++ EMS/Controllers/employeeDashboardController.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EMS.Models;
+
+namespace EMS.Controllers
+{
+    public class employeeDashboardController : Controller
+    {
+        private readonly EmployeedbContext _context;
+
+        public employeeDashboardController(EmployeedbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       public async Task<IActionResult> departmentDetails()
+        {
+            return _context.DepartmentTables != null ?
+                View(await _context.DepartmentTables.ToListAsync()):
+                Problem("Entity set 'EmployeeDbContext.DepartmentTables' is null.");
+        }
+
+        public async Task<IActionResult> designationDetails()
+        {
+            return _context.DesignationTables != null ?
+                View(await _context.DesignationTables.ToListAsync()):
+                Problem("Entity set 'EmployeeDbContext.DesignationTables' is null.");
+        }
+
+        public async Task<IActionResult> leaveDetails()
+        {
+            return _context.LeaveTables != null ?
+                View(await _context.LeaveTables.ToListAsync()) :
+                Problem("Entity set 'EmployeeDbContext.LeaveTables' is null.");
+        }
+
+        public async Task<IActionResult> vacancyDetails()
+        {
+            return _context.VacancyTables != null ?
+                View(await _context.VacancyTables.ToListAsync()) :
+                Problem("Entity set 'EmployeeDbContext.VacancyTables' is null.");
+        }
+
+
+    }
+}
---
+++ EMS/Controllers/userController.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EMS.Models;
+
+namespace EMS.Controllers
+{
+    public class userController : Controller
+    {
+        private readonly EmployeedbContext _context;
+
+        public userController(EmployeedbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: user
+        public async Task<IActionResult> Index()
+        {
+              return _context.UserTables != null ? 
+                          View(await _context.UserTables.ToListAsync()) :
+                          Problem("Entity set 'EmployeedbContext.UserTables'  is null.");
+        }
+
+        // GET: user/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.UserTables == null)
+            {
+                return NotFound();
+            }
+
+            var userTable = await _context.UserTables
+                .FirstOrDefaultAsync(m => m.UserId == id);
+            if (userTable == null)
+            {
+                return NotFound();
+            }
+
+            return View(userTable);
+        }
+
+        // GET: user/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: user/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("UserId,UserName,Password,Role")] UserTable userTable)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(userTable);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userTable);
+        }
+
+        // GET: user/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.UserTables == null)
+            {
+                return NotFound();
+            }
+
+            var userTable = await _context.UserTables.FindAsync(id);
+            if (userTable == null)
+            {
+                return NotFound();
+            }
+            return View(userTable);
+        }
+
+        // POST: user/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("UserId,UserName,Password,Role")] UserTable userTable)
+        {
+            if (id != userTable.UserId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(userTable);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserTableExists(userTable.UserId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userTable);
+        }
+
+        // GET: user/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.UserTables == null)
+            {
+                return NotFound();
+            }
+
+            var userTable = await _context.UserTables
+                .FirstOrDefaultAsync(m => m.UserId == id);
+            if (userTable == null)
+            {
+                return NotFound();
+            }
+
+            return View(userTable);
+        }
+
+        // POST: user/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.UserTables == null)
+            {
+                return Problem("Entity set 'EmployeedbContext.UserTables'  is null.");
+            }
+            var userTable = await _context.UserTables.FindAsync(id);
+            if (userTable != null)
+            {
+                _context.UserTables.Remove(userTable);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserTableExists(int id)
+        {
+          return (_context.UserTables?.Any(e => e.UserId == id)).GetValueOrDefault();
+        }
+        public ActionResult login() { return View(); }
+        [HttpPost]
+        public ActionResult login(string userName, string password)
+        {
+            var status = _context.UserTables.Where(m => m.UserName == userName && m.Password == password).FirstOrDefault();
+            if (status == null)
+            {
+                return View(); //Which means there is no user and we will stay on same page.
+            }
+            if (status.Role == "Admin")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+                return RedirectToAction("Index", "employeeDashboard");
+            //If the uset exist we will redirect to the Home controller and Index ActionResult.
+        }
+    }
+}
